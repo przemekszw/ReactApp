dbf64c66d2d39ee18d9389a0189ffbf2d1c3a27e
unknown <s223603@student.uek.krakow.pl>
2022-12-04 19:47:07 +0100

    04-12

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,17 @@
+import ExpenseDate from "./ExpenseDate";
+import Card from './Card';
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 3, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 203.23;
 
+function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
